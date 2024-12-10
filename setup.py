9a073c80e4e2bfa187421f902d0a0980d82eb323
Edoardo Balducci <edoardoba2004@gmail.com>
2024-12-10 12:44:20 +0100

    feat(readme): added shields

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ prompts_requirements = base_requirements.copy()
 
 setup(
     name="pyclack-cli",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     python_requires=">=3.7",
     
